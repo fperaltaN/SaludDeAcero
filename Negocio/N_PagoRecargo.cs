e255bad325bd4da9efda86568408f68b4d7664ea
fperaltaN <iscfperalta@gmail.com>
2015-12-30 02:22:51 -0600

    Clases de pagos y recargos

--- Negocio/N_PagoRecargo.cs
+++ Negocio/N_PagoRecargo.cs
@@ -15,44 +15,44 @@ using System.Threading.Tasks;
 
 namespace Negocio
 {
-    public class N_Pago
+    public class N_PagoRecargo
     {
         #region variables privadas
         private int transSucess = 0;
-        private string getPago = "sel_pago";
-        private string getPagoXId = "sel_byId_pago";
-        private string addPago = "add_pago";
-        private string updPago = "upd_pago";
-        private string delPago = "del_pago";
+        private string getPagoR = "sel_pago_recargo";
+        private string getPagoRXId = "sel_byId_pago_recargo";
+        private string addPagoR = "add_pago_recargo";
+        private string updPagoR = "upd_pago_recargo";
+        private string delPagoR = "del_pago_recargo";
         #endregion
 
         /// <summary>
         /// Obtiene todos los Pago de la BD
         /// </summary>
         /// <returns></returns>
-        public DataSet getPagoGrid()
+        public DataSet getPagoRecargoGrid()
         {
             SQLDatos obj = new SQLDatos();
             DataSet datos = new DataSet();
             int opSatisfactoria = 0;
-            opSatisfactoria = obj.getDataFromSP(getPago, "TblPago", datos);
+            opSatisfactoria = obj.getDataFromSP(getPagoR, "TblPago", datos);
             return datos;
         }
 
-         /// <summary>
+        /// <summary>
         /// Obtiene  la información de un Pago
         /// </summary>
         /// <param name="idPago"></param>
         /// <returns></returns>
-        public DataSet getPagoById(int idSocio)
+        public DataSet getPagoRecargoById(int idPago)
         {
             DataSet datos = new DataSet();
             SQLDatos obj = new SQLDatos();
             try
             {
                 SqlParameter[] param = new SqlParameter[1];
-                param[0] = new SqlParameter("@id_Socio", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, idSocio);
-               transSucess = obj.getDataFromSP(getPagoXId, param, "TblPago", datos);
+                param[0] = new SqlParameter("@id_pago", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, idPago);
+                transSucess = obj.getDataFromSP(getPagoRXId, param, "TblPago", datos);
             }
             catch (Exception ex)
             {
@@ -71,19 +71,16 @@ namespace Negocio
         /// <param name="importe"></param>
         /// <param name="idPago"></param>
         /// <returns></returns>
-        public int addPagos(int id_paquete, int id_socio, int id_empleado, int importe,ref int idPago)
+        public int addPagoRecargo(int idPago,int idRecargo)
         {
             try
             {
                 DataSet datos = new DataSet();
                 SQLDatos obj = new SQLDatos();
-                SqlParameter[] param = new SqlParameter[4];
-                param[0] = new SqlParameter("@id_paquete", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_paquete);
-                param[1] = new SqlParameter("@id_socio", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_socio);
-                param[2] = new SqlParameter("@id_empleado", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_empleado);
-                param[3] = new SqlParameter("@importe", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, importe);
-                transSucess = obj.getDataFromSP(addPago, param, "TblPago", datos);
-                idPago = Convert.ToInt32(datos.Tables[0].Rows[0]["id_pago"].ToString());
+                SqlParameter[] param = new SqlParameter[2];
+                param[0] = new SqlParameter("@id_pago", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, idPago);
+                param[1] = new SqlParameter("@id_recargo", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, idRecargo);
+               transSucess = obj.getDataFromSP(addPagoR, param, "TblPago", datos);
             }
             catch (Exception ex)
             {
@@ -102,19 +99,18 @@ namespace Negocio
         /// <param name="importe"></param>
         /// <param name="activo"></param>
         /// <returns></returns>
-        public int updtPagos(int id_paquete, int id_socio, int id_empleado, int importe , int activo)
+        public int updtPagoRecargo(int id_pago_recargo, int id_Pago, int id_recargo,int activo)
         {
             try
             {
                 DataSet datos = new DataSet();
                 SQLDatos obj = new SQLDatos();
-                SqlParameter[] param = new SqlParameter[5];
-                param[0] = new SqlParameter("@id_paquete", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_paquete);
-                param[1] = new SqlParameter("@id_socio", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_socio);
-                param[2] = new SqlParameter("@id_empleado", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_empleado);
-                param[3] = new SqlParameter("@importe", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, importe);
+                SqlParameter[] param = new SqlParameter[4];
+                param[0] = new SqlParameter("@id_pago_recargo", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_pago_recargo);
+                param[1] = new SqlParameter("@id_pago", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_Pago);
+                param[2] = new SqlParameter("@id_recargo", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_recargo);
                 param[4] = new SqlParameter("@activo", SqlDbType.Bit, 250, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, activo);
-                transSucess = obj.getDataFromSP(updPago, param, "TblPago", datos);
+                transSucess = obj.getDataFromSP(updPagoR, param, "TblPago", datos);
             }
             catch (Exception ex)
             {
@@ -128,15 +124,15 @@ namespace Negocio
         /// </summary>
         /// <param name="idPago"></param>
         /// <returns></returns>
-        public int DelPago(Int32 idPago)
+        public int DelPagoRecargo(Int32 idPago)
         {
             DataSet datos = new DataSet();
             SQLDatos obj = new SQLDatos();
             SqlParameter[] param = new SqlParameter[1];
             try
             {
-                param[0] = new SqlParameter("@id_Pago", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, idPago);//tenia idPago??
-                transSucess = obj.getDataFromSP(delPago, param, "TblPago", datos);
+                param[0] = new SqlParameter("@id_pago_recargo", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, idPago);//tenia idPago??
+                transSucess = obj.getDataFromSP(delPagoR, param, "TblPago", datos);
             }
             catch (Exception ex)
             {
--- Negocio/N_Recargo.cs
+++ Negocio/N_Recargo.cs
@@ -2,7 +2,7 @@
 '' Autor:	Faleg A. Peralta
 '' Modificado por: 
 '' Fecha de Modificación: 06.10.2015
-'' Descripcion General: CRUD de Pago
+'' Descripcion General: CRUD de recargo
 '' =============================================*/
 using CapaDatos;
 using System;
@@ -15,44 +15,44 @@ using System.Threading.Tasks;
 
 namespace Negocio
 {
-    public class N_Pago
+    public class N_Recargo
     {
         #region variables privadas
         private int transSucess = 0;
-        private string getPago = "sel_pago";
-        private string getPagoXId = "sel_byId_pago";
-        private string addPago = "add_pago";
-        private string updPago = "upd_pago";
-        private string delPago = "del_pago";
+        private string getRecargo = "sel_recargo";
+        private string getRecargoXId = "sel_byId_recargo";
+        private string addRecargo = "add_recargo";
+        private string updRecargo = "upd_recargo";
+        private string delRecargo = "del_recargo";
         #endregion
 
         /// <summary>
-        /// Obtiene todos los Pago de la BD
+        /// Obtiene todos los recargo de la BD
         /// </summary>
         /// <returns></returns>
-        public DataSet getPagoGrid()
+        public DataSet getRecargoGrid()
         {
             SQLDatos obj = new SQLDatos();
             DataSet datos = new DataSet();
             int opSatisfactoria = 0;
-            opSatisfactoria = obj.getDataFromSP(getPago, "TblPago", datos);
+            opSatisfactoria = obj.getDataFromSP(getRecargo, "Tblrecargo", datos);
             return datos;
         }
 
-         /// <summary>
-        /// Obtiene  la información de un Pago
+        /// <summary>
+        /// Obtiene  la información de un recargo
         /// </summary>
-        /// <param name="idPago"></param>
+        /// <param name="idrecargo"></param>
         /// <returns></returns>
-        public DataSet getPagoById(int idSocio)
+        public DataSet getRecargoById(int idSocio)
         {
             DataSet datos = new DataSet();
             SQLDatos obj = new SQLDatos();
             try
             {
                 SqlParameter[] param = new SqlParameter[1];
-                param[0] = new SqlParameter("@id_Socio", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, idSocio);
-               transSucess = obj.getDataFromSP(getPagoXId, param, "TblPago", datos);
+                param[0] = new SqlParameter("@id_recargo", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, idSocio);
+                transSucess = obj.getDataFromSP(getRecargoXId, param, "Tblrecargo", datos);
             }
             catch (Exception ex)
             {
@@ -63,15 +63,15 @@ namespace Negocio
         }
 
         /// <summary>
-        /// Agregalos Pago en la base de datos
+        /// Agregalos recargo en la base de datos
         /// </summary>
         /// <param name="id_paquete"></param>
         /// <param name="id_socio"></param>
-        /// <param name="id_empleado"></param>
+        /// <param name="liberacion"></param>
         /// <param name="importe"></param>
-        /// <param name="idPago"></param>
+        /// <param name="idrecargo"></param>
         /// <returns></returns>
-        public int addPagos(int id_paquete, int id_socio, int id_empleado, int importe,ref int idPago)
+        public int addRecargos(int id_paquete, int id_socio, Boolean liberacion,  int importe, ref int idrecargo)
         {
             try
             {
@@ -80,10 +80,10 @@ namespace Negocio
                 SqlParameter[] param = new SqlParameter[4];
                 param[0] = new SqlParameter("@id_paquete", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_paquete);
                 param[1] = new SqlParameter("@id_socio", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_socio);
-                param[2] = new SqlParameter("@id_empleado", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_empleado);
+                param[2] = new SqlParameter("@liberacion", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, liberacion);
                 param[3] = new SqlParameter("@importe", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, importe);
-                transSucess = obj.getDataFromSP(addPago, param, "TblPago", datos);
-                idPago = Convert.ToInt32(datos.Tables[0].Rows[0]["id_pago"].ToString());
+                transSucess = obj.getDataFromSP(addRecargo, param, "Tblrecargo", datos);
+                idrecargo = Convert.ToInt32(datos.Tables[0].Rows[0]["id_recargo"].ToString());
             }
             catch (Exception ex)
             {
@@ -94,7 +94,7 @@ namespace Negocio
         }
 
         /// <summary>
-        /// Actualiza la información de un Pago
+        /// Actualiza la información de un recargo
         /// </summary>
         /// <param name="id_paquete"></param>
         /// <param name="id_socio"></param>
@@ -102,19 +102,20 @@ namespace Negocio
         /// <param name="importe"></param>
         /// <param name="activo"></param>
         /// <returns></returns>
-        public int updtPagos(int id_paquete, int id_socio, int id_empleado, int importe , int activo)
+        public int updtRecargos(int idrecargo,int id_paquete, int id_socio, Boolean liberacion, int importe, int activo)
         {
             try
             {
                 DataSet datos = new DataSet();
                 SQLDatos obj = new SQLDatos();
-                SqlParameter[] param = new SqlParameter[5];
-                param[0] = new SqlParameter("@id_paquete", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_paquete);
-                param[1] = new SqlParameter("@id_socio", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_socio);
-                param[2] = new SqlParameter("@id_empleado", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_empleado);
-                param[3] = new SqlParameter("@importe", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, importe);
-                param[4] = new SqlParameter("@activo", SqlDbType.Bit, 250, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, activo);
-                transSucess = obj.getDataFromSP(updPago, param, "TblPago", datos);
+                SqlParameter[] param = new SqlParameter[6];
+                param[0] = new SqlParameter("@id_recargo", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_paquete);
+                param[1] = new SqlParameter("@id_paquete", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_paquete);
+                param[2] = new SqlParameter("@id_socio", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id_socio);
+                param[3] = new SqlParameter("@liberacion", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, liberacion);
+                param[4] = new SqlParameter("@importe", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, importe);
+                param[5] = new SqlParameter("@activo", SqlDbType.Bit, 250, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, activo);
+                transSucess = obj.getDataFromSP(updRecargo, param, "Tblrecargo", datos);
             }
             catch (Exception ex)
             {
@@ -126,17 +127,17 @@ namespace Negocio
         /// <summary>
         /// Elimina de la Base de Datos
         /// </summary>
-        /// <param name="idPago"></param>
+        /// <param name="idrecargo"></param>
         /// <returns></returns>
-        public int DelPago(Int32 idPago)
+        public int DelRecargo(Int32 idrecargo)
         {
             DataSet datos = new DataSet();
             SQLDatos obj = new SQLDatos();
             SqlParameter[] param = new SqlParameter[1];
             try
             {
-                param[0] = new SqlParameter("@id_Pago", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, idPago);//tenia idPago??
-                transSucess = obj.getDataFromSP(delPago, param, "TblPago", datos);
+                param[0] = new SqlParameter("@id_recargo", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, idrecargo);//tenia idrecargo??
+                transSucess = obj.getDataFromSP(delRecargo, param, "Tblrecargo", datos);
             }
             catch (Exception ex)
             {
