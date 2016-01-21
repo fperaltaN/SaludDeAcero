e37d2798f5f6bec164f11f956b14608982f47932
fperaltaN <iscfperalta@gmail.com>
2016-01-21 13:40:18 -0600

    modificacion de clase de negocio

--- Negocio/N_Paquete.cs
+++ Negocio/N_Paquete.cs
@@ -123,7 +123,7 @@ namespace Negocio
                 SQLDatos obj = new SQLDatos();
                 SqlParameter[] param = new SqlParameter[7];
                 param[0] = new SqlParameter("@id_paquete", SqlDbType.Int, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, idpaquete);
-                param[1] = new SqlParameter("@nombre", SqlDbType.VarChar, 10, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, nombre);
+                param[1] = new SqlParameter("@nombre", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, nombre);
                 param[2] = new SqlParameter("@descripcion", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, descripcion);
                 param[3] = new SqlParameter("@costo", SqlDbType.Decimal, 100, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, costo.Replace(",","."));
                 param[4] = new SqlParameter("@diasPaquete", SqlDbType.VarChar, 50, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, diasPaquete);
